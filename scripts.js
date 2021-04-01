a592516ddce753457a2c6adb03173ae0e4cca877
ryuk878 <J.hartsink@gmail.com>
2021-04-01 11:35:55 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,93 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'destionation-over'; 
+var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+const edge = 50;
+let drawing = false; 
+const mouse = {
+    x: null,
+    y: null
+}
+
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+
+})
+
+class Root {
+    constructor(x, y, color, centerX, centerY) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+    draw() {
+        this.speedX += (Math.random() - 0.5) / 2;
+        this.speedY += (Math.random() - 0.5) / 2;
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        const distanceX = this.x - this.centerX;
+        const distanceY = this.y - this.centerY;
+        const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+        const radius = (-distance / edge +1) * edge / 10; 
+        if (radius > 0) {
+            requestAnimationFrame(this.draw.bind(this));
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI);
+            ctx.fillStyle = this.color; 
+            ctx.fill(); 
+            ctx.strokeStyle = 'gold'
+            ctx.stroke();
+        }
+    }
+}
+
+function brachOut() {
+    if (drawing === true) {
+    const centerX = mouse.x;
+    const centerY = mouse.y;
+    for (let i = 0; i < 3 ; i++){
+    const root = new Root(mouse.x, mouse.y, 'blue', centerX, centerY); 
+    root.draw();
+    }
+}
+}
+window.addEventListener('resize', function(){
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+})
+
+window.addEventListener('mousemove', function(){
+    //ctx.fillStyle = 'rgba(255,255,255,0.03)';
+    //ctx.fillRect(0, 0, canvas.width, canvas.height); 
+    brachOut(); 
+});
+
+window.addEventListener('mousedown', function() {
+    drawing = true ;
+    
+
+}); 
+
+window.addEventListener('mouseup', function(){ 
+    drawing = false; 
+
+
+});
+
+function generateRandomColor()
+{
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+    
+}
+
